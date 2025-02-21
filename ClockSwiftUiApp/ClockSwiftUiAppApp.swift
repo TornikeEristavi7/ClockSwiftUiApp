03765febc4da4a41aa9634b1cc482e756b9c366e
Tornike <tornikeeristavi43@gmail.com>
2025-02-22 01:11:53 +0400

    Initial Commit

---
+++ ClockSwiftUiApp/ClockSwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockSwiftUiAppApp.swift
+//  ClockSwiftUiApp
+//
+//  Created by Tornike Eristavi on 22.02.25.
+//
+
+import SwiftUI
+
+@main
+struct ClockSwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
